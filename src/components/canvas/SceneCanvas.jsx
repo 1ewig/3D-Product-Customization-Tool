a03257c9e7c87c2c7ffef7097527e8dd9159afb4
Moshu <sellfor59@gmail.com>
2026-05-07 14:32:16 +0500

    style: remove 3D outline box and solid glass box around shirt model

--- src/components/canvas/SceneCanvas.jsx
+++ src/components/canvas/SceneCanvas.jsx
@@ -140,36 +140,7 @@ export const SceneCanvas = () => {
             <ShirtModel />
           </Center>
 
-          {/* Holographic 3D Solid Translucent Display Box */}
-          <mesh scale={[1.6, 1.6, 1.6]}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshPhysicalMaterial
-              color="#3b82f6"
-              transparent
-              opacity={0.03}
-              roughness={0.2}
-              metalness={0.1}
-              transmission={0.5}
-              thickness={0.3}
-              side={2} // THREE.DoubleSide
-              depthWrite={false}
-            />
-          </mesh>
 
-          {/* Holographic 3D Bounding Outline Box */}
-          <lineSegments scale={[1.6, 1.6, 1.6]}>
-            <edgesGeometry attach="geometry">
-              <boxGeometry args={[1, 1, 1]} />
-            </edgesGeometry>
-            <lineBasicMaterial
-              attach="material"
-              color="#3b82f6"
-              transparent
-              opacity={0.15}
-              depthWrite={false}
-              linewidth={1.5}
-            />
-          </lineSegments>
 
           {/* Subtly illuminated Ground Studio Grid */}
           <gridHelper
